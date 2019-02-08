6e8d1c0c91338fcf26435cb02124b9b15d7ed8dc
KontosTwo <li.vincent98@gmail.com>
2019-02-08 15:40:13 -0600

    redis ready for testing

--- snekboop-worker/src/worker.py
+++ snekboop-worker/src/worker.py
@@ -2,6 +2,7 @@ import redis
 import json
 import rpyc
 import os
+import asyncio
 
 worker_port = None
 
@@ -27,4 +28,50 @@ except:
     print("Failed initialization. The environment variable AWS_SECRET_ACCESS_KEY must be defined")
     exit(1)
 
+redis_host = None
 
+try:
+    redis_host = os.environ["REDIS_HOST"]
+except:
+    print("Failed initialization. The environment variable REDIS_HOST must be defined")
+    exit(1)
+
+redis_port = None
+
+try:
+    redis_port = os.environ["REDIS_PORT"]
+except:
+    print("Failed initialization. The environment variable REDIS_PORT must be defined")
+    exit(1)
+
+r = redis.Redis(host=redis_host, port=redis_port, db=0)
+
+
+class WorkerService(rpyc.Service):
+    def on_connect(self, conn):
+        pass
+
+    def on_disconnect(self, conn):
+        pass
+
+    def exposed_write(self, name, data):
+        r.execute_command('JSON.ARRAPPEND', name, '.', data)
+        pass
+
+    def exposed_query(self, name, func):
+        json_list = json.loads(r.execute_command('JSON.GET', name))
+        self.process_queries(json_list, func)
+        pass
+
+    async def process_queries(self, json_list, func):
+        loop = asyncio.get_event_loop()
+        futures = []
+        for json_object in json_list:
+            futures.append(func(json_object))
+        results = loop.run_until_complete(asyncio.gather(futures))
+        return results
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(WorkerService, port=int(worker_port))
+    t.start()
