f1fc79b9c18e555e5dbb61d29b2d9afff72a449c
KontosTwo <li.vincent98@gmail.com>
2019-02-18 15:56:20 -0600

    Redesign of project
    Api - handles requests and aggregates results from workers
    Meta - stores metadata on which name is mapped to which partition of Elasticache
    Worker - obtains the python function from S3 and executes it on its Elasticache partition data
    S3 - stores python functions
    Elasticache - stores the json list

--- snekboop-aggregator/src/runner.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t snekboop-aggregator .
-docker run snekboop-aggregator -e AWS_ACCESS_KEY_ID=1e2esg24g AWS_SECRET_ACCESS_KEY=asdf786asg876 SNEKBOOP_AGGREGATOR_PORT=10001
