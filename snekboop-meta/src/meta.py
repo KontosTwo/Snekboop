c661f3d8783de6d1007e2205ba2cf9d1bc1bf817
KontosTwo <li.vincent98@gmail.com>
2019-02-19 14:35:15 -0600

    fixed gitignore

--- snekboop-meta/src/meta.py
+++ snekboop-meta/src/meta.py
@@ -2,29 +2,29 @@ import json
 import rpyc
 import os
 import boto3
+import logging
+
+logger = logging.getLogger()
 
 meta_port = None
-
 try:
     meta_port = os.environ["SNEKBOOP_META_PORT"]
 except:
-    print("Failed initialization. The environment variable SNEKBOOP_META_PORT must be defined")
+    logger.error("Failed initialization. The environment variable SNEKBOOP_META_PORT must be defined")
     exit(1)
 
 access_key = None
-
 try:
     access_key = os.environ["AWS_ACCESS_KEY_ID"]
 except:
-    print("Failed initialization. The environment variable AWS_ACCESS_KEY_ID must be defined")
+    logger.error("Failed initialization. The environment variable AWS_ACCESS_KEY_ID must be defined")
     exit(1)
 
 secret_key = None
-
 try:
     secret_key = os.environ["AWS_SECRET_ACCESS_KEY"]
 except:
-    print("Failed initialization. The environment variable AWS_SECRET_ACCESS_KEY must be defined")
+    logger.error("Failed initialization. The environment variable AWS_SECRET_ACCESS_KEY must be defined")
     exit(1)
 
 
