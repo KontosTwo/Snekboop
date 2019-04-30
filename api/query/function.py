dbb4464ae1c12fc5f28f6cf1924c3eb51a59d47d
KontosTwo <li.vincent98@gmail.com>
2019-04-30 17:23:45 -0500

    almost working query

--- api/query/function.py
+++ api/query/function.py
@@ -1,49 +1,72 @@
 import os
 import json
-import requests
 import aioredis
 import asyncio
+import requests
+import requests_async
+
 
 def handler(event, context):
     loop = asyncio.get_event_loop()
 
     meta_find_url = os.environ["META_FIND_URL"]
+    meta_find_function_url = os.environ["META_FIND_FUNCTION_URL"]
 
     name = event["name"]
-    data = event["data"]
+    function = event["function"]
 
-    post_json ={
+    post_json = {
         "name": name
     }
     params = {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
     }
-    shards = requests.post(url=meta_find_url,json=post_json,params=params).json()["body"]
-    loop.run_until_complete(write_job(loop, name, shards, data))
+    shards = requests.post(url=meta_find_url, json=post_json, params=params).json()["body"]
+
+    post_json2 = {
+        "func_name": function
+    }
+    params2 = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url=meta_find_function_url, json=post_json2, params=params2).json()
+    function_url = response['body']
+
+    data = loop.run_until_complete(query_job(loop, name, shards, function_url))
 
     message = {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": "Successfully written to snekboop"
+        "body": json.dumps(data)
     }
     return message
 
-async def write_job(loop,name, shards,data):
-    write_promises = []
+
+async def query_job(loop, name, shards, function_url):
     conns = []
-    conn_close_promises = []
+    results = []
+    final_result = []
+
     for shard in shards:
         shard_dict = json.loads(shard)
         conn = await aioredis.create_redis(
             (shard_dict["host"], shard_dict["port"]))
-        multi = conn.multi_exec()
-        for datum in data:
-            multi.lpush(name,json.dumps(datum))
-        conns.append(conn)
-        write_promises.append(multi.execute())
+        data = await conn.lrange(name, 0, -1)
 
-    for write_promise in write_promises:
-        await write_promise
+        post_json = {
+            "data": data
+        }
+        params = {
+            "Content-Type": "application/json"
+        }
+        result = requests_async.post(url=function_url, json=post_json, params=params)
+        conns.append(conn)
+        results.append(result)
+
+    for result in results:
+        print(result)
 
     for conn in conns:
         conn.close()
+
+    return final_result
