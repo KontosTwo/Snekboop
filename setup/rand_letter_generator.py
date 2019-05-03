72314395afed3175e4f4b786c86b2cf932fb2188
Chris <chris@iAzul.T-mobile.com>
2019-05-03 17:25:01 -0500

    changed output to one long string instead of a list of letters

--- setup/rand_letter_generator.py
+++ setup/rand_letter_generator.py
@@ -8,13 +8,14 @@ def gen_rand_letters():
 
     legal_chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-    data = []
+    data = ""
 
     i = 0
     while i < 1000000:
         index = random.randint(0, 25)
         #myfile.write("test")
-        data.append(legal_chars[index])
+        #data.append(legal_chars[index])
+        data += legal_chars[index]
         i += 1
 
     write_call("random_letters", data)
