ecb8f21608a437ce9a13f490427be8685c52cbbe
Chris <chris@iAzul.T-mobile.com>
2019-05-03 17:07:01 -0500

    changed rand letter gen file

--- setup/rand_letter_generator.py
+++ setup/rand_letter_generator.py
@@ -1,23 +1,23 @@
 #Returns a list of randomly generated letters
 import random
 
-import lib.api_calls
+from lib.api_calls import *
 
+def gen_rand_letters():
+    #myfile = open("random_letters.txt", "w")
 
-#myfile = open("random_letters.txt", "w")
+    legal_chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-legal_chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    data = []
 
-data = []
+    i = 0
+    while i < 1000000:
+        index = random.randint(0, 25)
+        #myfile.write("test")
+        data.append(legal_chars[index])
+        i += 1
 
-i = 0
-while i < 1000000:
-    index = random.randint(0, 25)
-    #myfile.write("test")
-    data.append(legal_chars[index])
-    i += 1
-
-write_call("random_letters", data)
-#print(output_list)
-#return output_list
+    write_call("random_letters", data)
+    #print(output_list)
+    #return output_list
 
